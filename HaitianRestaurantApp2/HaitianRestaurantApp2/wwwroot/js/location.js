fc01552f3e12df64453a312083437b338d81586b
Mervens Val <valmervens@gmail.com>
2021-08-09 17:13:30 -0400

    Completed CRUD UI

--- HaitianRestaurantApp2/HaitianRestaurantApp2/wwwroot/js/location.js
+++ HaitianRestaurantApp2/HaitianRestaurantApp2/wwwroot/js/location.js
@@ -1,24 +1,3 @@
 ﻿$(document).ready(function () {
-    $('#menuEmployee').DataTable({
-        initComplete: function () {
-            this.api().columns([2]).every(function () {
-                var column = this;
-                var select = $('<select class="class="form-control form-control-sm"><option value="">All Categories</option></select>')
-                    .appendTo('#categoryDropdown')
-                    .on('change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
-
-                        column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                    });
-
-                column.data().unique().sort().each(function (d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
-                });
-            });
-        }
-    });
+    $('#locationsTable').DataTable();
 });
